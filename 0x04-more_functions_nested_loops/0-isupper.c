11459cc8721c879835ce3b0615f8a91f72ea6c5d
Odunayo <jegedeo172@gmail.com>
2022-12-17 03:57:39 -0800

    uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,14 @@
 
 /**
  * _isupper - Check for uppercase character
- *
- * Return: 1 if c is uppercase or 0 otherwise
+ * @c: int to be checked
+ * Return: 1 if uppercase, otherwise 0
  */
-int _isupper()
+int _isupper(int c)
 {
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+	if (c >= 64 && c <= 91)
+	{
+	return (1);
+	}
+	return (0);
 }
